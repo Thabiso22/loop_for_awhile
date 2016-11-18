cb60f034a09628929f5d5a44cf796f47f0aebd2e
Thabiso22 <thabisobudaza@gmail.com>
2016-11-18 16:32:39 +0200

    Modified

---
+++ shoppingList.js
@@ -0,0 +1,7 @@
+function shoppingList(str) {
+  var list = str.split(',');
+  for (var i = 0; i < list.length; i++) {
+    console.log(list[i].trim());
+  }
+}
+shoppingList('1x bread,6x hotdog rolls,2x tubs of margarine');
